a700e51d84faff38225d160c9e57ae5154a3949f
Maksim Makhanko <tolkoo.keeper@gmail.com>
2024-05-05 02:00:26 +0300

    refactor: rearrange annotations

--- src/main/java/by/grsu/skydiving/adapter/in/web/AuthController.java
+++ src/main/java/by/grsu/skydiving/adapter/in/web/AuthController.java
@@ -1,7 +1,7 @@
 package by.grsu.skydiving.adapter.in.web;
 
-import by.grsu.skydiving.adapter.in.web.dto.SignInRequest;
-import by.grsu.skydiving.adapter.in.web.dto.SignInResponse;
+import by.grsu.skydiving.adapter.in.web.request.SignInRequest;
+import by.grsu.skydiving.adapter.in.web.response.SignInResponse;
 import by.grsu.skydiving.adapter.in.web.mapper.AuthMapper;
 import by.grsu.skydiving.application.port.in.SignInUseCase;
 import by.grsu.skydiving.common.WebAdapter;
@@ -9,9 +9,9 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
+@WebAdapter
 @RestController
 @RequestMapping("api/v1/auth")
-@WebAdapter
 @RequiredArgsConstructor
 public class AuthController {
     private final SignInUseCase signInUseCase;
