ef00eace3438f956f42f095bf170c06e19eb9211
Maksim Makhanko <tolkoo.keeper@gmail.com>
2024-05-10 00:27:53 +0300

    feat: save new skydiver, get skydiver page

--- src/main/java/by/grsu/skydiving/adapter/out/persistence/repository/SkydiverPersistenceAdapter.java
+++ src/main/java/by/grsu/skydiving/adapter/out/persistence/repository/SkydiverPersistenceAdapter.java
@@ -1,5 +1,6 @@
 package by.grsu.skydiving.adapter.out.persistence.repository;
 
+import by.grsu.skydiving.adapter.out.persistence.UserInfoJdbcRepository;
 import by.grsu.skydiving.adapter.out.persistence.entity.PassportInfoEntity;
 import by.grsu.skydiving.adapter.out.persistence.entity.SkydiverEntity;
 import by.grsu.skydiving.adapter.out.persistence.entity.UserInfoEntity;
