71636aafcd3794eecd582d69a45dea362e8a8cd0
Maksim Makhanko <tolkoo.keeper@gmail.com>
2024-05-14 18:07:53 +0300

    feat: docker

--- src/main/java/by/grsu/skydiving/adapter/in/web/CompetitionController.java
+++ src/main/java/by/grsu/skydiving/adapter/in/web/CompetitionController.java
@@ -1,14 +1,20 @@
 package by.grsu.skydiving.adapter.in.web;
 
 import by.grsu.skydiving.adapter.in.web.mapper.CompetitionMapper;
+import by.grsu.skydiving.adapter.in.web.mapper.TeamMapper;
 import by.grsu.skydiving.adapter.in.web.request.AddStageRequest;
 import by.grsu.skydiving.adapter.in.web.request.InitiateCompetitionRequest;
+import by.grsu.skydiving.adapter.in.web.request.TeamRequest;
 import by.grsu.skydiving.adapter.in.web.response.AddStageResponse;
 import by.grsu.skydiving.adapter.in.web.response.InitiateCompetitionResponse;
+import by.grsu.skydiving.adapter.in.web.response.TeamResponse;
 import by.grsu.skydiving.application.domain.model.competition.Competition;
 import by.grsu.skydiving.application.domain.model.competition.CompetitionStage;
+import by.grsu.skydiving.application.domain.model.competition.Team;
 import by.grsu.skydiving.application.port.in.AddStageToCompetitionUseCase;
 import by.grsu.skydiving.application.port.in.AddStageToCompetitionUseCase.AddStageCommand;
+import by.grsu.skydiving.application.port.in.AddTeamToCompetitionUseCase;
+import by.grsu.skydiving.application.port.in.AddTeamToCompetitionUseCase.AddTeamToCompetitionCommand;
 import by.grsu.skydiving.application.port.in.InitiateCompetitionUseCase;
 import by.grsu.skydiving.application.port.in.InitiateCompetitionUseCase.InitiateCompetitionCommand;
 import by.grsu.skydiving.common.WebAdapter;
@@ -23,13 +29,15 @@ import org.springframework.web.bind.annotation.*;
 public class CompetitionController {
     private final InitiateCompetitionUseCase initiateUseCase;
     private final AddStageToCompetitionUseCase addStageUseCase;
-    private final CompetitionMapper mapper;
+    private final AddTeamToCompetitionUseCase addTeamUseCase;
+    private final CompetitionMapper competitionMapper;
+    private final TeamMapper teamMapper;
 
     @PostMapping("/initial")
     @ResponseStatus(HttpStatus.CREATED)
     public InitiateCompetitionResponse initiateCompetitionCreation(@RequestBody
                                                                    InitiateCompetitionRequest request) {
-        InitiateCompetitionCommand command = mapper.toCommand(request);
+        InitiateCompetitionCommand command = competitionMapper.toCommand(request);
         Competition competition = initiateUseCase.initiateCompetition(command);
 
         return new InitiateCompetitionResponse(competition.getId());
@@ -37,13 +45,29 @@ public class CompetitionController {
 
     @PostMapping("/{competitionId}/stage")
     @ResponseStatus(HttpStatus.CREATED)
-    public AddStageResponse addStageToCompetition(@PathVariable
-                                                  Long competitionId,
-                                                  @RequestBody
-                                                  AddStageRequest request) {
-        AddStageCommand command = mapper.toCommand(competitionId, request);
+    public AddStageResponse addStageToCompetition(
+            @PathVariable
+            Long competitionId,
+            @RequestBody
+            AddStageRequest request
+    ) {
+        AddStageCommand command = competitionMapper.toCommand(competitionId, request);
         CompetitionStage stage = addStageUseCase.addStage(command);
 
         return new AddStageResponse(competitionId, stage.id());
+    }
+
+    @PostMapping("/{competitionId}/team")
+    @ResponseStatus(HttpStatus.CREATED)
+    public TeamResponse addCommandToCompetition(
+            @PathVariable
+            Long competitionId,
+            @RequestBody
+            TeamRequest request
+    ) {
+        AddTeamToCompetitionCommand command = teamMapper.toCommand(competitionId, request);
+        Team team = addTeamUseCase.addTeam(command);
+
+        return new TeamResponse(team.id());
     }
 }
---
+++ src/main/java/by/grsu/skydiving/adapter/out/persistence/repository/TeamPersistenceAdapter.java
@@ -0,0 +1,90 @@
+package by.grsu.skydiving.adapter.out.persistence.repository;
+
+import by.grsu.skydiving.adapter.out.persistence.entity.CompetitionMemberDetailsEntity;
+import by.grsu.skydiving.adapter.out.persistence.entity.TeamEntity;
+import by.grsu.skydiving.adapter.out.persistence.mapper.TeamEntityMapper;
+import by.grsu.skydiving.application.domain.model.competition.Competition;
+import by.grsu.skydiving.application.domain.model.competition.Team;
+import by.grsu.skydiving.application.port.out.ExistsTeamByNamePort;
+import by.grsu.skydiving.application.port.out.SaveCompetitionTeamsPort;
+import by.grsu.skydiving.common.PersistenceAdapter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.*;
+
+@PersistenceAdapter
+@RequiredArgsConstructor
+public class TeamPersistenceAdapter implements SaveCompetitionTeamsPort,
+        ExistsTeamByNamePort {
+    private final TeamJdbcRepository teamRepository;
+    private final CompetitionMemberDetailsJdbcRepository membersRepository;
+    private final TeamEntityMapper mapper;
+
+    @Override
+    @Transactional
+    public List<Team> saveTeams(Competition competition) {
+        long competitionId = competition.getId();
+
+        List<Team> unsavedTeams = competition.getTeams();
+        List<TeamEntity> teamsEntities = mapper.toEntities(unsavedTeams);
+        teamsEntities = teamRepository.saveAll(teamsEntities);
+        List<Team> saveTeams = new ArrayList<>();
+        for (TeamEntity teamEntity: teamsEntities) {
+            Team unsavedTeam = unsavedTeams.stream()
+                    .filter(team -> team.name().equals(teamEntity.getName()))
+                    .findAny()
+                    .get();
+
+            saveTeams.add(unsavedTeam.withId(teamEntity.getId()));
+        }
+
+        List<CompetitionMemberDetailsEntity> members = saveTeams.stream()
+                .flatMap(team -> mapper.toMembers(team, competitionId).stream())
+                .toList();
+        members = saveMembers(members);
+
+        return mapToDomains(teamsEntities, members);
+    }
+
+    @Override
+    public boolean exists(String teamName) {
+        return teamRepository.existsByName(teamName);
+    }
+
+    private List<CompetitionMemberDetailsEntity> saveMembers(List<CompetitionMemberDetailsEntity> members) {
+        return membersRepository.saveAll(members);
+    }
+
+    private List<Team> mapToDomains(List<TeamEntity> teams, List<CompetitionMemberDetailsEntity> members) {
+        Map<Long, Set<CompetitionMemberDetailsEntity>> membersGroupedByTeamId = groupByTeamId(members);
+
+        return teams.stream()
+                .map(teamEntity -> {
+                    Long teamId = teamEntity.getId();
+                    Set<CompetitionMemberDetailsEntity> membersOfTeam = membersGroupedByTeamId.get(teamId);
+                    return mapper.toDomain(teamEntity, membersOfTeam);
+                })
+                .toList();
+    }
+
+    private Map<Long, Set<CompetitionMemberDetailsEntity>> groupByTeamId(List<CompetitionMemberDetailsEntity> members) {
+        Map<Long, Set<CompetitionMemberDetailsEntity>> membersGroupedByTeamId = HashMap.newHashMap(5);
+
+        for (CompetitionMemberDetailsEntity memberDetails : members) {
+            long teamId = memberDetails.getTeamId();
+
+            Set<CompetitionMemberDetailsEntity> membersOfTeam;
+            if (!membersGroupedByTeamId.containsKey(teamId)) {
+                membersOfTeam = new HashSet<>();
+                membersGroupedByTeamId.put(teamId, membersOfTeam);
+            } else {
+                membersOfTeam = membersGroupedByTeamId.get(teamId);
+            }
+
+            membersOfTeam.add(memberDetails);
+        }
+
+        return membersGroupedByTeamId;
+    }
+}
