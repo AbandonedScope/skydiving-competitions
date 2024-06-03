55652b306e6c4f6fc05a4a58b59410d3749c9715
mahanko.maksim <mahanko.maksim@wb.ru>
2024-06-03 22:33:27 +0300

    fix: поправил, что id возвращалось null fix: роль для регистрации теперь указывается строкой

--- src/main/java/by/grsu/skydiving/application/port/out/SaveUserPort.java
+++ src/main/java/by/grsu/skydiving/application/port/out/SaveUserPort.java
@@ -3,5 +3,5 @@ package by.grsu.skydiving.application.port.out;
 import by.grsu.skydiving.application.domain.model.auth.UserAuthInfo;
 
 public interface SaveUserPort {
-    void save(UserAuthInfo userAuthInfo);
+    long save(UserAuthInfo userAuthInfo);
 }
