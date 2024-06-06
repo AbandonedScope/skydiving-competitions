508ee3bf873d499aa76c0312e16cb8436b4d0205
mahanko.maksim <mahanko.maksim@wb.ru>
2024-06-06 23:48:39 +0300

    нус, попробуем

--- src/main/java/by/grsu/skydiving/adapter/in/web/mapper/TrickSerieResponseMapper.java
+++ src/main/java/by/grsu/skydiving/adapter/in/web/mapper/TrickSerieResponseMapper.java
@@ -10,6 +10,7 @@ import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 import org.mapstruct.MappingConstants;
 import org.mapstruct.ReportingPolicy;
+import java.util.List;
 
 @Mapper(
     componentModel = MappingConstants.ComponentModel.SPRING,
