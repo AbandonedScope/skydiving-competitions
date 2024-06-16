a0eef57ed55c0c4f55715ba3df109a7a1bd823a8
mahanko.maksim <mahanko.maksim@wb.ru>
2024-06-16 15:05:00 +0300

    fix: поправил билд

--- src/main/java/by/grsu/skydiving/application/domain/model/common/FilteringFieldsNames.java
+++ src/main/java/by/grsu/skydiving/application/domain/model/common/FilteringFieldsNames.java
@@ -1,12 +1,12 @@
 package by.grsu.skydiving.application.domain.model.common;
 
-import lombok.experimental.UtilityClass;
+public final class FilteringFieldsNames {
+    public static final String NAME_FILTER = "name";
+    public static final String GENDER_FILTER = "gender";
+    public static final String SPORT_RANK_FILTER = "sportRank";
+    public static final String IS_INTERNAL_FILTER = "isInternal";
+    public static final String CATEGORY_FILTER = "category";
 
-@UtilityClass
-public class FilteringFieldsNames {
-    public final String NAME_FILTER = "name";
-    public final String GENDER_FILTER = "gender";
-    public final String SPORT_RANK_FILTER = "sportRank";
-    public final String IS_INTERNAL_FILTER = "isInternal";
-    public final String CATEGORY_FILTER = "category";
+    private FilteringFieldsNames() {
+    }
 }
