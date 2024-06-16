0d931341b02599bc635866964e5bbc994bc22284
mahanko.maksim <mahanko.maksim@wb.ru>
2024-06-16 18:00:07 +0300

    feat: добавил судейские категории в knowledge-base

--- src/main/java/by/grsu/skydiving/common/knowledge/KnowledgeBaseUtils.java
+++ src/main/java/by/grsu/skydiving/common/knowledge/KnowledgeBaseUtils.java
@@ -1,5 +1,6 @@
 package by.grsu.skydiving.common.knowledge;
 
+import by.grsu.skydiving.application.domain.model.competition.RefereeCategory;
 import by.grsu.skydiving.application.domain.model.skydiver.SportRank;
 import java.util.Arrays;
 import java.util.HashMap;
@@ -10,14 +11,26 @@ import lombok.experimental.UtilityClass;
 @UtilityClass
 public class KnowledgeBaseUtils {
     private static final String SPORT_RANKS = "sportRanks";
+    private static final String REFEREE_CATEGORIES = "refereeCategory";
 
 
     public Map<String, List<EnumModel>> getEnums() {
         Map<String, List<EnumModel>> enums = HashMap.newHashMap(3);
 
         enums.put(SPORT_RANKS, getSportRanks());
+        enums.put(REFEREE_CATEGORIES, getRefereeCategories());
 
         return enums;
+    }
+
+    private List<EnumModel> getRefereeCategories() {
+        return Arrays.stream(RefereeCategory.values())
+            .map(value -> EnumModel.builder()
+                .id(value.getId())
+                .name(value.name())
+                .description(value.getDescription())
+                .build())
+            .toList();
     }
 
     private List<EnumModel> getSportRanks() {
@@ -25,7 +38,7 @@ public class KnowledgeBaseUtils {
             .map(value -> EnumModel.builder()
                 .id(value.getId())
                 .name(value.name())
-                .description(value.getRussianName())
+                .description(value.getDescription())
                 .build())
             .toList();
     }
