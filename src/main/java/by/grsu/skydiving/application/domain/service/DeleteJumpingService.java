444d50914e39c4107b7691a1239c5b5616f259b8
mahanko.maksim <mahanko.maksim@wb.ru>
2024-06-20 18:37:38 +0300

    feat: нельзя начать судейство соревнования, которое ещё не началось

--- src/main/java/by/grsu/skydiving/application/domain/service/DeleteJumpingService.java
+++ src/main/java/by/grsu/skydiving/application/domain/service/DeleteJumpingService.java
@@ -4,7 +4,7 @@ import by.grsu.skydiving.application.domain.model.jumping.JumpingInfo;
 import by.grsu.skydiving.application.domain.model.jumping.NextJumpingNumber;
 import by.grsu.skydiving.application.port.in.DeleteJumpingUseCase;
 import by.grsu.skydiving.application.port.in.GetCompetitionJumpingUseCase;
-import by.grsu.skydiving.application.port.in.GetUpdatableCompetitionUseCase;
+import by.grsu.skydiving.application.port.in.GetRefereeableCompetitionUseCase;
 import by.grsu.skydiving.application.port.out.DeleteJumpingPort;
 import by.grsu.skydiving.application.port.out.GetNextNumberOfJumpingPort;
 import by.grsu.skydiving.common.UseCase;
@@ -13,14 +13,14 @@ import lombok.RequiredArgsConstructor;
 @UseCase
 @RequiredArgsConstructor
 public class DeleteJumpingService implements DeleteJumpingUseCase {
-    private final GetUpdatableCompetitionUseCase getUpdatableCompetitionUseCase;
+    private final GetRefereeableCompetitionUseCase getRefereeableCompetitionUseCase;
     private final GetCompetitionJumpingUseCase getCompetitionJumpingUseCase;
     private final GetNextNumberOfJumpingPort getNextNumberOfJumpingPort;
     private final DeleteJumpingPort deleteJumpingPort;
 
     @Override
     public void delete(long competitionId, long jumpingId) {
-        getUpdatableCompetitionUseCase.getCompetitionThatCanBeUpdated(competitionId);
+        getRefereeableCompetitionUseCase.getCompetitionThatCanBeRefereed(competitionId);
         JumpingInfo jumpingInfo = getCompetitionJumpingUseCase.getJumpingInfo(jumpingId);
         NextJumpingNumber nextNumber =
             getNextNumberOfJumpingPort.genNextNumberOfJumping(competitionId, jumpingInfo.skydiverId());
